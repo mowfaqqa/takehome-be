424232d6b0c926ea843582f4bf31854a210c0fff
muwaffaq <itzmuwaffaq@gmail.com>
2023-07-26 22:08:31 +0100

    implemented the image upload endpoint

--- controllers/formController.js
+++ controllers/formController.js
@@ -1,19 +1,25 @@
 const admin = require("firebase-admin");
+const storage = admin.storage().bucket();
 
 exports.submitData = async (req, res) => {
   const db = admin.firestore();
 
-  const { company, numUsers, numProducts, percentage } = req.body;
+  const { company, numUsers, numProducts, percentage, email } = req.body;
 
   try {
+    const authorizedEmail = "admin@email.com";
+    if (email !== authorizedEmail) {
+      return res.status(403).json({ error: "Unauthorized to upload image" });
+    }
     // Create a new document in the 'data' collection
-    await db.collection("data").add({
+    const dataRef = db.collection("data").doc();
+    await dataRef.set({
       company,
       numUsers,
       numProducts,
       percentage,
+      imageUrl: "",
     });
-
     res.status(200).json({ message: "Data submitted successfully" });
   } catch (error) {
     console.error("Error submitting data:", error);
@@ -21,6 +27,42 @@ exports.submitData = async (req, res) => {
   }
 };
 
+exports.uploadImage = async (req, res) => {
+  const {dataId} = req.params;
+  const { email } = req.body
+  const authorizedEmail = "admin@email.com";
+
+  try {
+    // Check if the email is authorized to upload images
+    if (email !== authorizedEmail) {
+      return res.status(403).json({ error: 'Unauthorized to upload image' });
+    }
+
+    const dataRef = db.collection('data').doc(dataId);
+
+    if (!req.file) {
+      return res.status(400).json({ error: 'No image provided' });
+    }
+
+    const imageFile = req.file;
+    const imageUrl = `images/${dataId}_${imageFile.originalname}`;
+
+    // Upload the image to Firebase Storage
+    await storage.file(imageUrl).save(imageFile.buffer, {
+      metadata: {
+        contentType: imageFile.mimetype,
+      },
+    });
+
+    // Update the 'imageUrl' field in the Firestore document
+    await dataRef.update({ imageUrl });
+
+    res.status(200).json({ message: 'Image uploaded successfully' });
+  } catch (error) {
+    console.error('Error uploading image:', error);
+    res.status(500).json({ error: 'Image upload failed' });
+  }
+}
 exports.fetchData = async (req, res) => {
   const db = admin.firestore();
   try {
--- routes/formRoutes.js
+++ routes/formRoutes.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { submitData, fetchData } = require("../controllers/formController");
+const {
+  submitData,
+  fetchData,
+  uploadImage,
+} = require("../controllers/formController");
 
 // endpoint for User A and User B to submit data
 router.post("/submit", submitData);
 
 // endpoint for user C to fetch data
 router.get("/data", fetchData);
+// endpoint for User C to upload an image
+router.post("/upload/:dataId", uploadImage);
 
 module.exports = router;
