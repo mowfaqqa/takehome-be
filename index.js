07b34545d76d5d6e9badf36ab5b50468340826d1
muwaffaq <itzmuwaffaq@gmail.com>
2023-07-26 21:02:06 +0100

    updated node

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const mongoose = require("mongoose");
 const authRoute = require("./routes/authRoute");
 const formRoute = require("./routes/formRoutes");
 
